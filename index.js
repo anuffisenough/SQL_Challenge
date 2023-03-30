4486007a8b56405ee9228d2861dbe2fe9fe8cd05
anuffisenough <uffmanch@gmail.com>
2023-03-30 14:36:11 -0600

    adds functionality for subsequent questions, responses

--- index.js
+++ index.js
@@ -23,25 +23,84 @@ app.use((req, res) => {
 })
 
 
-// const questions = [
-//     { 
-//         type: 'list',
-//         message: "What would you like to do?",
-//         name: "todos",
-//         choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
-//     },
-// ];
-
-
-function toDoQuestions() {
-inquirer.prompt([
+const firstQuestion = [
     { 
         type: 'list',
         message: "What would you like to do?",
         name: "todos",
         choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+    }
+];
+
+const employeeQuestions = [
+    { 
+        type: 'input',
+        message: "What is the employee's first name?",
+        name: "employee_first",
     },
-])
+    { 
+        type: 'input',
+        message: "What is the employee's last name?",
+        name: "employee_last",
+    },
+    { 
+        type: 'list',
+        message: "What is the employee's role?",
+        name: "employee_role",
+        choices: ['test role 1', 'test role 2', 'test role 3', 'test role 4']
+    },
+    { 
+        type: 'list',
+        message: "Who is the employee's manager?",
+        name: "employee_manager",
+        choices: ['test manager 1', 'test manager 2', 'test manager 3', 'test manager 4']
+    }
+];
+
+const roleQuestions = [
+    { 
+        type: 'input',
+        message: "What is the name of the role?",
+        name: "role_name",
+    },
+    {
+        type: 'input',
+        message: 'What is the salaray of the role?',
+        name: "role_salary",
+    },
+    { 
+        type: 'list',
+        message: "What department does the role belong to?",
+        name: "role_department",
+        choices: ['test dept 1', 'test dept 2', 'test dept 3', 'test dept 4']
+    }
+];
+
+const departmentQuestion = [
+    {
+        type: 'input',
+        message: 'What is the name of the department?',
+        name: 'department_name'
+    }
+];
+
+const employeeUpdateQuestions = [
+    {
+        type: 'list',
+        message: "Which employee's role would you like to update?",
+        name: 'employee_update_name',
+        choices: ['test name 1', 'test name 2', 'test name 3', 'test name 4']
+    },
+    {
+        type: 'list',
+        message: 'Which role do you want to assign the selected employee?',
+        name: 'employee_update_role',
+        choices: ['test role 1', 'test role 2', 'test role 3', 'test role 4']
+    }
+];
+
+function askQuestions() {
+inquirer.prompt(firstQuestion)
 .then((response) => {
     const result = response.todos;
     switch(result) {
@@ -49,55 +108,58 @@ inquirer.prompt([
             db.query('SELECT * FROM employee', function (err, response) {
             console.table(response)});
             break;
-            // toDoQuestions();
 
         case 'View All Roles':
             db.query('SELECT * FROM role', function (err, response) {
-            console.table(response)});
+            console.table(response)
+            });
+            // askQuestions();
             break;
-            // toDoQuestions();
 
         case 'View All Departments':
             db.query('SELECT * FROM department', function (err, response) {
-            console.table(response)});
+            console.table(response)
+            });
+            // askQuestions();
             break;
-            // toDoQuestions();
 
-        case 'View All Roles':
-            db.query('SELECT * FROM role', function (err, response) {
-            console.table(response)});
+        case 'Add Employee':
+            inquirer
+                .prompt(employeeQuestions).then((response) => {
+            console.log(`Added ${response.employee_first} ${response.employee_last} to the database.`);
+            });      
+            // askQuestions();
             break;
-            // toDoQuestions();
+        
+        case 'Add Role':
+            inquirer
+                .prompt(roleQuestions).then((response) => {
+                console.log(`Added ${response.role_name} to the database.`);
+            });      
+            break;
+        
+        case 'Add Department':
+            inquirer
+                .prompt(departmentQuestion).then((response) => {
+                console.log(`Added ${response.department_name} to the database.`);
+            });      
+            break;
+        
+        case 'Update Employee Role':
+            inquirer
+                .prompt(employeeUpdateQuestions).then((response) => {
+                console.log("Updated employee's role.");
+            });      
+            break;
 
         case 'Quit':
             console.log("Goodbye");
-        
-        // default: 
-        //     toDoQuestions();
-  
-            }      
-
-  toDoQuestions();
+            break;
+            }            
+    
         })
-        
+
     };
 
-    toDoQuestions();
+askQuestions();
 
-
-    
-// 
-//     case "View All Employess":
-    //      console.table(db.query('SELECT * FROM employee', function (err, response));
-    //     break;
-    // case "View All Roles":
-    //     db.query('SELECT * FROM role', function (err, response) {
-    //     console.table(response);
-    //     });
-    //     break;
-    // case "View All Departments":
-    //     db.query('SELECT * FROM department', function (err, response) {
-    //     console.table(response);
-    //     });
-    //     break;
-    // }
