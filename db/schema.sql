23b3008d4ca50410f7cce8e2d88fea68948553f0
anuffisenough <uffmanch@gmail.com>
2023-03-31 20:36:55 -0600

    fixes errors from askBCS

--- db/schema.sql
+++ db/schema.sql
@@ -12,9 +12,7 @@ CREATE TABLE role (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
-  ON DELETE SET NULL
+  department_id INT
 );
 
 CREATE TABLE employee (
--- index.js
+++ index.js
@@ -125,8 +125,8 @@ function askEmployeeQuestions() {
     db.query("INSERT INTO employee SET ?", {
         first_name: response.employee_first,
         last_name: response.employee_last,
-        // role_id: response.employee_role,
-        // manager_id: response.employee_manager,
+        // role_id: response.role_id,
+        // manager_id: response.manager_id,
     })
     console.log(`Added ${response.employee_first} ${response.employee_last} to the database.`)
     askQuestions();
