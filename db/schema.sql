62a5df0ac06a9c816f7b8ca7640427e375236ff1
anuffisenough <uffmanch@gmail.com>
2023-03-31 20:17:50 -0600

    adjusted questions function

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,9 @@ CREATE TABLE role (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
-  department_id INT
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
